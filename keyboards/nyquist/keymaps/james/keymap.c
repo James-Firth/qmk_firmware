4add242364a6621ee06909918cf2184dc2f842ff
James Firth <james.firth@norimaconsulting.com>
2017-08-21 00:06:19 -0500

    Uses proper mod keys. Removed adjust layer tap function. wasn't working.
    Added i3wm hotkeys to ADJUST layer
    
    Removed tap dance

--- keyboards/nyquist/keymaps/james/keymap.c
+++ keyboards/nyquist/keymaps/james/keymap.c
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+   +------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+   +------+------+------+------+------+------|
- * | Ctrl |Adjust| Alt  | GUI  |Lower |Space |   |Space |Raise | Adjust| alt | shift|RCtrl |
+ * | Ctrl | PgDn | Alt  | GUI  |Lower |Space |   |Space |Raise | Adjust| alt | shift|RCtrl |
  * `-----------------------------------------+   +-----------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  MT(KC_LSFT, LGUI(KC_D)), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  MT(KC_LCTL, KC_HOME), MT(ADJUST, KC_PGDN),  MT(KC_LALT ,KC_PGUP), MT(KC_LGUI, KC_END), LOWER,   KC_SPC,  KC_SPC,  RAISE,   MT(ADJUST, KC_LEFT),  MT(KC_RALT, KC_DOWN), MT(KC_RSFT, KC_UP), MT(KC_RCTRL, KC_RGHT) \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  CTL_T(KC_HOME),   KC_PGDN,  ALT_T(KC_PGUP), GUI_T(KC_END), LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT,  MT(MOD_RALT, KC_DOWN), MT(MOD_RSFT, KC_UP), MT(MOD_RCTL, KC_RGHT) \
 ),
 
 /* Colemak
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL, \
   KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  CTL_T(KC_HOME),   KC_PGDN,  ALT_T(KC_PGUP), GUI_T(KC_END), LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT,  MT(MOD_RALT, KC_DOWN), MT(MOD_RSFT, KC_UP), MT(MOD_RCTL, KC_RGHT) \
 ),
 
 /* Maintenance/Upkeep layer
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  KEYMAP( \
   QWERTY,   COLEMAK,   UPKEEP, _______,  _______, _______, _______,   _______,  _______,   _______,  _______,  _______, \
-  _______, KC_F1,     KC_F2,   KC_F3,   KC_F4, RGB_HUI, _______, _______,  KC_PGUP, _______, _______, _______, \
+  _______, KC_F1,     KC_F2,   KC_F3,   KC_F4, RGB_HUI, _______, LGUI(LSFT(KC_P4)),  KC_PGUP, LGUI(LSFT(KC_P6)), _______, _______, \
   KC_DEL,  KC_F5,     KC_F6,   KC_F7,   KC_F8, _______, _______, KC_HOME,  KC_PGDN, KC_END,  _______, KC_RCTRL, \
   _______, KC_F9,     KC_F10,  KC_F11,  KC_F12, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
