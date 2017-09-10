a7b7876ea53c2a775b4cfe51b8faa48c52904708
James Firth <james.firth@norimaconsulting.com>
2017-09-09 20:00:17 -0500

    WIP gaming layer

--- keyboards/nyquist/keymaps/james/keymap.c
+++ keyboards/nyquist/keymaps/james/keymap.c
@@ -17,6 +17,7 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  GAME,
   COLEMAK,
   UPKEEP,
   LOWER,
@@ -46,9 +47,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_MEH, KC_QUOT), \
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   CTL_T(KC_HOME),   KC_PGDN,  ALT_T(KC_PGUP), GUI_T(KC_END), LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT,  MT(MOD_RALT, KC_DOWN), MT(MOD_RSFT, KC_UP), MT(MOD_RCTL, KC_RGHT) \
+),
+
+/* Gaming TODO WIP
+ * ,-----------------------------------------+   +-----------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+   +------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * |------+------+------+------+------+------+   +------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------+   +------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+   +------+------+------+------+------+------|
+ * | Ctrl | PgDn | Alt  | GUI  |Lower |Space |   |Space |Raise | Adjust| alt | shift|RCtrl |
+ * `-----------------------------------------+   +-----------------------------------------'
+ */
+[_GAME] = KEYMAP( \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_MEH, KC_QUOT), \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LCTL, LOWER, _______, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  RAISE,  KC_LEFT,  MT(MOD_RALT, KC_DOWN), MT(MOD_RSFT, KC_UP), MT(MOD_RCTL, KC_RGHT) \
 ),
 
 /* Colemak
@@ -137,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise) TODO Implement comments
  * ,-----------------------------------------+   +-----------------------------------------.
- * |Qwerty|Colemk|UPkeep|      |      |      |   |      |      |      |      |      |
+ * |Qwerty|Colemk|UPkeep|GAME  |      |      |   |      |      |      |      |      |
  * |------+------+------+------+------+------+   +------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |      |   |      |  MvL | PgUp | MvR  |      |      |
  * |------+------+------+------+------+------+   +------+------+------+------+------+------|
@@ -149,7 +171,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------+   +-----------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
-  QWERTY,   COLEMAK,   UPKEEP, _______,  _______, _______, _______,   _______,  _______,   _______,  _______,  _______, \
+  QWERTY,   COLEMAK,   UPKEEP, GAME,  _______, _______, _______,   _______,  _______,   _______,  _______,  _______, \
   _______, KC_F1,     KC_F2,   KC_F3,   KC_F4, RGB_HUI, _______, LGUI(LSFT(KC_P4)),  KC_PGUP, LGUI(LSFT(KC_P6)), _______, _______, \
   KC_DEL,  KC_F5,     KC_F6,   KC_F7,   KC_F8, _______, _______, KC_HOME,  KC_PGDN, KC_END,  _______, KC_RCTRL, \
   _______, KC_F9,     KC_F10,  KC_F11,  KC_F12, _______, _______, _______, _______, _______, _______, _______, \
